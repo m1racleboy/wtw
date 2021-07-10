f6a5651d0fcbc4d241788df0b5a1811c41e4ee1c
Nikita <miracleboy3228@mail.ru>
2021-07-10 16:45:40 +0300

    Меняет рендер звездочек на экране добавления комментария

--- project/src/components/add-review-form-screen/add-review-form-screen.jsx
+++ project/src/components/add-review-form-screen/add-review-form-screen.jsx
@@ -7,6 +7,7 @@ import { useInput } from '../../hooks/useInput';
 
 const COMMENT_MIN_LENGTH = 50;
 const COMMENT_MAX_LENGTH = 400;
+const STARS_LENGTH = 10;
 export function AddReviewFormScreen(props) {
   const { onSubmit } = props;
   const { id } = useParams();
@@ -25,74 +26,31 @@ export function AddReviewFormScreen(props) {
     onSubmit(state);
   };
 
+  const stars = Array.from({ length: STARS_LENGTH }, (v, i) => i + 1).reverse();
+
+  const createStarsTemplate = (starValue) => (
+    <React.Fragment key={starValue}>
+      <input
+        className="rating__input"
+        id={`star-${starValue}`}
+        type="radio" name="rating"
+        value={starValue}
+        onChange={(e) => rating.onChange(e)}
+        onBlur={(e) => rating.onBlur(e)}
+      />
+      <label className="rating__label" htmlFor={`star-${starValue}`}>Rating {starValue}</label>
+    </React.Fragment>
+  );
+
   return (
     <div className="add-review">
       <form onSubmit={handleSubmit} action="#" className="add-review__form">
-        <div className="rating" style={{ display: 'flex', 'flex-direction': 'column' }}>
+        <div className="rating" style={{ display: 'flex', flexDirection: 'column' }}>
           {rating.isEmpty && <div style={{ color: 'black', margin: '0 auto' }}>Укажите рейтинг фильма</div>}
           <div className="rating__stars">
-            <input className="rating__input" id="star-10" type="radio" name="rating" value="10"
-              onChange={(e) => rating.onChange(e)}
-              onBlur={(e) => rating.onBlur(e)}
-            />
-            <label className="rating__label" htmlFor="star-10">Rating 10</label>
-
-            <input className="rating__input" id="star-9" type="radio" name="rating" value="9"
-              onChange={(e) => rating.onChange(e)}
-              onBlur={(e) => rating.onBlur(e)}
-            />
-            <label className="rating__label" htmlFor="star-9">Rating 9</label>
-
-            <input className="rating__input" id="star-8" type="radio" name="rating" value="8"
-              onChange={(e) => rating.onChange(e)}
-              onBlur={(e) => rating.onBlur(e)}
-            />
-            <label className="rating__label" htmlFor="star-8">Rating 8</label>
-
-            <input className="rating__input" id="star-7" type="radio" name="rating" value="7"
-              onChange={(e) => rating.onChange(e)}
-              onBlur={(e) => rating.onBlur(e)}
-            />
-            <label className="rating__label" htmlFor="star-7">Rating 7</label>
-
-            <input className="rating__input" id="star-6" type="radio" name="rating" value="6"
-              onChange={(e) => rating.onChange(e)}
-              onBlur={(e) => rating.onBlur(e)}
-            />
-            <label className="rating__label" htmlFor="star-6">Rating 6</label>
-
-            <input className="rating__input" id="star-5" type="radio" name="rating" value="5"
-              onChange={(e) => rating.onChange(e)}
-              onBlur={(e) => rating.onBlur(e)}
-            />
-            <label className="rating__label" htmlFor="star-5">Rating 5</label>
-
-            <input className="rating__input" id="star-4" type="radio" name="rating" value="4"
-              onChange={(e) => rating.onChange(e)}
-              onBlur={(e) => rating.onBlur(e)}
-            />
-            <label className="rating__label" htmlFor="star-4">Rating 4</label>
-
-            <input className="rating__input" id="star-3" type="radio" name="rating" value="3"
-              onChange={(e) => rating.onChange(e)}
-              onBlur={(e) => rating.onBlur(e)}
-            />
-            <label className="rating__label" htmlFor="star-3">Rating 3</label>
-
-            <input className="rating__input" id="star-2" type="radio" name="rating" value="2"
-              onChange={(e) => rating.onChange(e)}
-              onBlur={(e) => rating.onBlur(e)}
-            />
-            <label className="rating__label" htmlFor="star-2">Rating 2</label>
-
-            <input className="rating__input" id="star-1" type="radio" name="rating" value="1"
-              onChange={(e) => rating.onChange(e)}
-              onBlur={(e) => rating.onBlur(e)}
-            />
-            <label className="rating__label" htmlFor="star-1">Rating 1</label>
+            {stars.map((starValue) => createStarsTemplate(starValue))}
           </div>
         </div>
-
         <div className="add-review__text">
           {(comment.isDirty && comment.isEmpty) && <div style={{ color: 'black' }}>Комментарий не может быть пустым</div>}
           {(comment.isDirty && comment.minLengthError) && <div style={{ color: 'black' }}>Слишком короткий комментарий, осталось: {COMMENT_MIN_LENGTH - comment.value.length}</div>}
@@ -114,7 +72,7 @@ export function AddReviewFormScreen(props) {
 
         </div>
       </form>
-    </div>
+    </div >
   );
 }
 
