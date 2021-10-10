c696550176749494348eea085d5f3a62e3267ddf
Nikita <miracleboy3228@mail.ru>
2021-10-10 18:08:19 +0300

    deploy try

--- server.js
+++ server.js
@@ -6,12 +6,8 @@ const app = express();
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/ping', function (req, res) {
-  return res.send('pong');
-});
+app.get('/ping', (req, res) => res.send('pong'));
 
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+app.get('/*', (req, res) => res.sendFile(path.join(__dirname, 'build', 'index.html')));
 
 app.listen(port);
